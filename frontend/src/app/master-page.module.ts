f36cc2a37a68617ed70a62f6760e4ac7fba96b49
Shreyank <shreyank@designstring.com>
2024-06-21 18:55:23 +0530

    faucet page enabled

--- frontend/src/app/master-page.module.ts
+++ frontend/src/app/master-page.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Routes, RouterModule, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import {
+  Routes,
+  RouterModule,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+} from '@angular/router';
 import { MasterPageComponent } from './components/master-page/master-page.component';
 import { SharedModule } from './shared/shared.module';
 
@@ -12,7 +17,7 @@ import { BlocksList } from './components/blocks-list/blocks-list.component';
 import { RbfList } from './components/rbf-list/rbf-list.component';
 import { ServerHealthComponent } from './components/server-health/server-health.component';
 import { ServerStatusComponent } from './components/server-health/server-status.component';
-import { FaucetComponent } from './components/faucet/faucet.component'
+import { FaucetComponent } from './components/faucet/faucet.component';
 
 const browserWindow = window || {};
 // @ts-ignore
@@ -26,7 +31,7 @@ const routes: Routes = [
       {
         path: 'mining/blocks',
         redirectTo: 'blocks',
-        pathMatch: 'full'
+        pathMatch: 'full',
       },
       {
         path: 'tx/push',
@@ -42,7 +47,8 @@ const routes: Routes = [
       },
       {
         path: 'about',
-        loadChildren: () => import('./components/about/about.module').then(m => m.AboutModule),
+        loadChildren: () =>
+          import('./components/about/about.module').then((m) => m.AboutModule),
       },
       {
         path: 'blocks',
@@ -54,99 +60,114 @@ const routes: Routes = [
       },
       {
         path: 'terms-of-service',
-        loadChildren: () => import('./components/terms-of-service/terms-of-service.module').then(m => m.TermsOfServiceModule),
+        loadChildren: () =>
+          import('./components/terms-of-service/terms-of-service.module').then(
+            (m) => m.TermsOfServiceModule
+          ),
       },
       {
         path: 'privacy-policy',
-        loadChildren: () => import('./components/privacy-policy/privacy-policy.module').then(m => m.PrivacyPolicyModule),
+        loadChildren: () =>
+          import('./components/privacy-policy/privacy-policy.module').then(
+            (m) => m.PrivacyPolicyModule
+          ),
       },
       {
         path: 'trademark-policy',
-        loadChildren: () => import('./components/trademark-policy/trademark-policy.module').then(m => m.TrademarkModule),
+        loadChildren: () =>
+          import('./components/trademark-policy/trademark-policy.module').then(
+            (m) => m.TrademarkModule
+          ),
       },
       {
         path: 'tx',
         component: StartComponent,
         data: { preload: true, networkSpecific: true },
-        loadChildren: () => import('./components/transaction/transaction.module').then(m => m.TransactionModule),
+        loadChildren: () =>
+          import('./components/transaction/transaction.module').then(
+            (m) => m.TransactionModule
+          ),
       },
       {
         path: 'block',
         component: StartComponent,
         data: { preload: true, networkSpecific: true },
-        loadChildren: () => import('./components/block/block.module').then(m => m.BlockModule),
+        loadChildren: () =>
+          import('./components/block/block.module').then((m) => m.BlockModule),
       },
       {
         path: 'docs',
-        loadChildren: () => import('./docs/docs.module').then(m => m.DocsModule),
+        loadChildren: () =>
+          import('./docs/docs.module').then((m) => m.DocsModule),
         data: { preload: true },
       },
       {
         path: 'api',
-        loadChildren: () => import('./docs/docs.module').then(m => m.DocsModule)
+        loadChildren: () =>
+          import('./docs/docs.module').then((m) => m.DocsModule),
       },
       {
         path: 'lightning',
-        loadChildren: () => import('./lightning/lightning.module').then(m => m.LightningModule),
-        data: { preload: browserWindowEnv && browserWindowEnv.LIGHTNING === true, networks: ['bitcoin'] },
+        loadChildren: () =>
+          import('./lightning/lightning.module').then((m) => m.LightningModule),
+        data: {
+          preload: browserWindowEnv && browserWindowEnv.LIGHTNING === true,
+          networks: ['bitcoin'],
+        },
       },
       {
         path: 'tools/calculator',
-        component: CalculatorComponent
+        component: CalculatorComponent,
+      },
+      {
+        path: 'faucet',
+        component: FaucetComponent,
       },
     ],
-  }
+  },
 ];
 
 if (window['__env']?.OFFICIAL_MEMPOOL_SPACE) {
   routes[0].children.push({
     path: 'monitoring',
     data: { networks: ['bitcoin', 'liquid'] },
-    component: ServerHealthComponent
+    component: ServerHealthComponent,
   });
   routes[0].children.push({
     path: 'nodes',
     data: { networks: ['bitcoin', 'liquid'] },
-    component: ServerStatusComponent
+    component: ServerStatusComponent,
   });
   if (window['isMempoolSpaceBuild']) {
     routes[0].children.push({
       path: 'faucet',
-      canActivate: [(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
-        return state.url.startsWith('/testnet4/');
-      }],
+      canActivate: [
+        (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+          return state.url.startsWith('/testnet4/');
+        },
+      ],
       component: StartComponent,
       data: { preload: true, networkSpecific: true },
-      children: [{
-        path: '',
-        data: { networks: ['bitcoin'] },
-        component: FaucetComponent,
-      }]
-    })
+      children: [
+        {
+          path: '',
+          data: { networks: ['bitcoin'] },
+          component: FaucetComponent,
+        },
+      ],
+    });
   }
 }
 
 @NgModule({
-  imports: [
-    RouterModule.forChild(routes)
-  ],
-  exports: [
-    RouterModule
-  ]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
-export class MasterPageRoutingModule { }
+export class MasterPageRoutingModule {}
 
 @NgModule({
-  imports: [
-    CommonModule,
-    MasterPageRoutingModule,
-    SharedModule,
-  ],
-  declarations: [
-    MasterPageComponent,
-  ],
-  exports: [
-    MasterPageComponent,
-  ]
+  imports: [CommonModule, MasterPageRoutingModule, SharedModule],
+  declarations: [MasterPageComponent],
+  exports: [MasterPageComponent],
 })
-export class MasterPageModule { }
+export class MasterPageModule {}
